11eb295c8bedcbeee78b6dd091139423f72fdf35
Egorka <trololo221@bk.ru>
2023-03-14 16:28:17 +0300

    Delete property file and method

--- src/test/java/homework1/GoogleTest.java
+++ src/test/java/homework1/GoogleTest.java
@@ -3,18 +3,12 @@ package homework1;
 import com.codeborne.selenide.Condition;
 import org.junit.Test;
 
-import java.util.Properties;
-
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import java.io.*;
 
 public class GoogleTest {
 
     @Test
-    public void userCanSearch() throws IOException {
-//        Properties properties = readPropertiesFile("src.main.resources");
-
+    public void userCanSearch() {
         open("http://ok.ru");
         OkLoginPage results = new OkLoginPage();
         results.getEmail().setValue("teddi2x2@gmail.com");
@@ -22,23 +16,5 @@ public class GoogleTest {
         results.getButtonToLogIn().click();
         OkProfilePage profile = new OkProfilePage();
         profile.getProfileName().shouldHave(Condition.text("Егор Топорков"));
-//        results.getResult(0).shouldHave(text("Selenide: concise UI tests in Java"));
-    }
-
-    public static Properties readPropertiesFile(String fileName) throws IOException {
-        FileInputStream fis = null;
-        Properties prop = null;
-        try {
-            fis = new FileInputStream(fileName);
-            prop = new Properties();
-            prop.load(fis);
-        } catch(FileNotFoundException fnfe) {
-            fnfe.printStackTrace();
-        } catch(IOException ioe) {
-            ioe.printStackTrace();
-        } finally {
-            fis.close();
-        }
-        return prop;
     }
 }
